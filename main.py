8c2b5b5860780626f4efe123bf4bcd6266b47b9b
Tomas Solc <tomas.solc@fs.cvut.cz>
2025-11-27 00:01:07 +0100

    implementation of grid with dimensions of max_attempts * word_length, the grid shows when player clicks on the buttons, only vizualization of grid nothing else yet

--- main.py
+++ main.py
@@ -34,10 +34,44 @@ class Button():
     def clicked_button(self, event):
         return event.type == pygame.MOUSEBUTTONDOWN and self.shape.collidepoint(event.pos) # collidepoint checks if the click of a mouse was inside of the shape or not, if not than returns False
 
+class WordleGameUI():
+    def __init__(self, screen):
+        self.screen = screen
+
+    def start_game_grid(self, word_length, max_attempts):
+
+        # implementation how the grid should look
+        rect_size = 60
+        padding = 10 # spaces between rectangles
+        start_x = (1280 - (rect_size + padding) * word_length) // 2
+        start_y = 100
+        
+        for row in range(max_attempts):
+            for col in range(word_length):
+                rect = pygame.Rect(
+                    start_x + col * (rect_size + padding),
+                    start_y + row * (rect_size + padding),
+                    rect_size,
+                    rect_size
+                ) # creates a 60x60 rectangle which is copied till word_length is reached which is collum and than this is done till max_attempts are reached which represents rows
+                pygame.draw.rect(self.screen, (200,200,200), rect, 3)
+
+        self.word_length = word_length
+        self.max_attempts = max_attempts
+
+
 # creation of buttons
 btn4 = Button("4 LETTER GAME", (50, 100, 200), 180, 50, (1280-180)/2, 200)
 btn5 = Button("5 LETTER GAME", (50, 100, 200), 180, 50, (1280-180)/2, 300)
 btn6 = Button("6 LETTER GAME", (50, 100, 200), 180, 50, (1280-180)/2, 400)
+
+# creation of grid
+grid = WordleGameUI(screen)
+
+# when the program is started and player did not choose target word length
+# used for vizualization from window with buttons to window with grid
+selected_max_attempts = None
+selected_word_length = None
 
 # main loop to keep the program running
 while running_program:
@@ -50,33 +84,49 @@ while running_program:
             # just a tests right now to check if the buttons still work correctly
             print("Start 4-letter game")
 
+            # test that game shows grid after click on button "4 LETTER GAME"
+            selected_word_length = 4
+            selected_max_attempts = 5
+
             # starts the game of Wordle with target word with 4 letters
-            game4 = game_4letter()
-            game4.evaluate_guess()
+            # game4 = game_4letter()
+            # game4.evaluate_guess()
 
         if btn5.clicked_button(event):
 
             # just a tests right now to check if the buttons still work correctly
             print("Start 5-letter game")
 
+            # test that game shows grid after click on button "5 LETTER GAME"
+            selected_word_length = 5
+            selected_max_attempts = 6
+
             # starts the game of Wordle with target word with 5 letters
-            game5 = game_5letter()
-            game5.evaluate_guess()
+            # game5 = game_5letter()
+            # game5.evaluate_guess()
 
         if btn6.clicked_button(event):
 
             # just a tests right now to check if the buttons still work correctly
             print("Start 6-letter game")
 
+            # test that game shows grid after click on button "6 LETTER GAME"
+            selected_word_length = 6
+            selected_max_attempts = 7
+
             # starts the game of Wordle with target word with 6 letters
-            game6 = game_6letter()
-            game6.evaluate_guess()
+            # game6 = game_6letter()
+            # game6.evaluate_guess()
 
+    screen.fill((30,30,30)) # fills the screen with a color, background
 
-    screen.fill((30,30,30)) # fills the screen with a color
-    btn4.draw(screen)
-    btn5.draw(screen)
-    btn6.draw(screen)
+    if selected_word_length is None: # first the player needs to set a length of target word to move onto guessing grid
+        btn4.draw(screen)
+        btn5.draw(screen)
+        btn6.draw(screen)
+    else:
+        grid.start_game_grid(selected_word_length, selected_max_attempts) # draws the grid
+
     pygame.display.flip() # finally displays everything drawn in this frame
 
 
